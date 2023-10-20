aa8f2e5a7cd8e4e04f0113b3ad8dc0564c15845e
SHIGWAN ADITYA ANANT <aditya.shigwan18@gmail.com>
2023-10-20 23:18:51 +0530

    state getting reinitialized

---
+++ app/src/main/java/com/example/composecalculator/CalculatorActions.kt
@@ -0,0 +1,13 @@
+package com.example.composecalculator
+
+sealed class CalculatorActions{
+    data class Number(val number: Int): CalculatorActions()
+
+    object Clear: CalculatorActions()
+    object Delete: CalculatorActions()
+    object Decimal: CalculatorActions()
+    object Calculate: CalculatorActions()
+
+    data class Operation(val calculatorOperation: CalculatorOperation):CalculatorActions()
+
+}
